2623526f3105d9d831d57ecc0cc8e431cc9edd3f
Alejandro Linan <jandrolinan@gmail.com>
2021-01-20 16:36:04 -0600

    products class written

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+From tomcat:9.0
+Maintainer product-retrieval
+COPY target/products.war /usr/local/tomcat/webapps/
--- src/main/java/product/Product.java
+++ src/main/java/product/Product.java
@@ -1,5 +1,53 @@
 package product;
 
-public class Product {
+import javax.persistence.*;
+import java.io.Serializable;
 
+public class Product implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "name")
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "type")
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	@Column(name = "price")
+	private long price;
+	
+	public long getPrice() {
+		return price;
+	}
+	
+	public void setPrice(long price) {
+		this.price = price;
+	}
 }
